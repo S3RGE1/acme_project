922e2b17849485e3a07d9db35333155a30c61586
Sergei Sirotkin <sergei-ant@yandex.ru>
2024-03-17 23:37:04 +0300

    full

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,10 +3,36 @@ from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
+
 
 urlpatterns = [
+    path(
+        'auth/registration/', 
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Подключаем дебаг-панель:
+if settings.DEBUG:
+    import debug_toolbar
+# Добавить к списку urlpatterns список адресов
+# из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+# Подключаем функцию static() к urlpatterns:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = 'core.views.page_not_found'
--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'birthday'
 
 urlpatterns = [
+    path('<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('login_only/', views.simple_view),
     path('', views.BirthdayCreateView.as_view(), name='create'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
     path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
